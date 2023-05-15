30ee773e61c4b9eae3b9b7de16c5185b494e2cbd
weikang zhou <57825291+nitwtog@users.noreply.github.com>
2023-05-15 15:07:49 +0800

    Update 日历天气

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import json
 from datetime import date, datetime
-import math
 from wechatpy import WeChatClient
 from wechatpy.client.api import WeChatMessage, WeChatTemplate
 import requests
@@ -8,71 +8,88 @@ import random
 
 today = datetime.now()
 start_date = os.environ['START_DATE']
-city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
-
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
-
 user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
-
-api_id = os.environ["API_ID"]
-api_secret = os.environ["API_SECRET"]
-
+tianqi_id = os.environ["tianqi_ID"]
+tianqi_secret = os.environ["tianqi_SECRET"]
+rili_key = os.environ["rili_key"]
+news_key = os.environ["news_key"]
 
 def get_holiday():
-    url = "https://www.mxnzp.com/api/holiday/single/"+str(today.date()).replace('-','') +f"?app_id={api_id}&app_secret={api_secret}"
-    res = requests.get(url).json()
-    holiday = res['data']
-    result = {"日期":holiday['date'],"星期几": holiday['weekDay'],"天干地支":holiday['yearTips'], "节日":holiday['typeDes'], "宜":holiday['suit'], "忌":holiday['avoid'], "这一年的第几天":holiday['dayOfYear'],"节气":holiday['solarTerms']}
-    return result
+    # date ='-'.join(str(int(i)) for i in str(today.date()).split('-'))
+    date = str(today.date())
+    url = f"http://apis.juhe.cn/fapig/calendar/day?date={date}&detail=1&key={rili_key}"
+    head = {'Content-Type': 'application/x-www-form-urlencoded'}
+    res = requests.get(url, headers=head).json()
+    return res['result']
 
 def get_weather():
-  url = "https://www.mxnzp.com/api/weather/current/" + city + f"?app_id={api_id}&app_secret={api_secret}"
-  res = requests.get(url).json()
-  weather = res['data']
-  return weather['weather'], str(weather['temp'])+"度"
+    headers = {
+        'User-Agent': 'Apifox/1.0.0 (https://www.apifox.cn)'
+    }
+    url = f"https://v0.yiketianqi.com/api?appid={tianqi_id}&appsecret={tianqi_secret}&version=v61&city=%E4%B8%8A%E6%B5%B7&province=%E4%B8%8A%E6%B5%B7"
+    res = requests.get(url, headers=headers)
+    result = json.loads(res.content)
+    return result
+
 
 def get_count():
-  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
-  return delta.days
+    delta = today - datetime.strptime(start_date, "%Y-%m-%d")
+    return delta.days
 
+def get_news():
+    type = 'caijing'
+    page = "1"
+    page_size = "10"
+
+    url = f"http://v.juhe.cn/toutiao/index?key={news_key}&type={type}&page={page}&page_size={page_size}"
+    head = {'Content-Type': 'application/x-www-form-urlencoded'}
+    res = requests.get(url, headers=head).json()
+    return res['result']
 def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-  if next < datetime.now():
-    next = next.replace(year=next.year + 1)
-  return (next - today).days
+    next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+    if next < datetime.now():
+        next = next.replace(year=next.year + 1)
+    return (next - today).days
+
 
 def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
-  if words.status_code != 200:
-    return get_words()
-  return words.json()['data']['text']
+    words = requests.get("https://api.shadiao.pro/chp")
+    if words.status_code != 200:
+        return get_words()
+    return words.json()['data']['text']
+
 
 def get_random_color():
-  return "#%06x" % random.randint(0, 0xFFFFFF)
+    return "#%06x" % random.randint(0, 0xFFFFFF)
 
 
-client = WeChatClient(app_id, app_secret)
-
-wm = WeChatMessage(client)
-wea, temperature = get_weather()
+weather = get_weather()
 holiday = get_holiday()
-data = {"weather":{"value":wea},
-        "temperature":{"value":temperature},
-        "love_days":{"value":get_count()},
-        "birthday_left":{"value":get_birthday()},
-        "words":{"value":get_words(), "color":get_random_color()},
-        "Today":{"value":holiday['日期']},
-        "Week":{"value":holiday['星期几']},
-        "holiday":{"value":holiday['节日']},
-        "suit":{"value":holiday['宜']},
-        "avoid":{"value":holiday['忌']},
-        "dayOfYear":{"value":str(holiday['这一年的第几天'])+"天"},
-        "solarTerms":{"value":holiday['节气']},
+# news = get_news()
+data = {"now_temp": {"value": weather['tem']},  # 当前天气
+        "max_temp": {"value": weather['tem1']},
+        "min_temp": {"value": weather['tem2']},
+        "weather": {"value": weather['wea']},
+        "air_tips": {"value": weather['air_tips']},
+        "air_level": {"value": weather['air_level']},
+        "love_days": {"value": get_count()},
+        "birthday_left": {"value": get_birthday()},
+        "words": {"value": get_words(), "color": get_random_color()},
+        "Today": {"value": holiday['date']},
+        "Week": {"value": holiday['week']},
+        "holiday": {"value": holiday['value']},
+        "suit": {"value": holiday['suit']},
+        "avoid": {"value": holiday['avoid']},
+        "statusDesc": {'value': holiday['statusDesc']},
+        "news":{"value":" \n".join([f"{k+1}. "+i['title'] for k,i in enumerate(news["data"])])}
         }
 print(data)
+
+client = WeChatClient(app_id, app_secret)
+wm = WeChatMessage(client)
 res = wm.send_template(user_id, template_id, data)
 print(res)
-
